b8ad5308e96bc439c1a03e80b80eb68d78aa0c41
oliveiradeigor <lrmcpe22@gmail.com>
2022-10-06 02:16:50 -0300

    criando a lógica para substrair os minutos e criando funcionalidade nos botões

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,31 +10,81 @@ const telaMinutos = document.querySelector('#minuto');
 const telaDivisor = document.querySelector('#divisor');
 
 
-let segundos = 0;
-let minutos = 0;
+const bntIniciar = document.querySelector('.start');
+const bntStop = document.querySelector('.stop');
+
+bntStop.style.display = "none";
+const bntNext = document.querySelector('.next');
 
 
-function segundo(){
-    segundos++
+let segundos = 60;
+let minutos = 24;
 
-    
-    if(segundos == 60){
-        minutos++
+/*
+function segundo() {
+    segundos--
+
+    if (segundos == 60) {
+        minutos--
         telaMinutos.innerHTML = minutos;
         segundos = 0;
     }
 
-    if(segundos <= 9){
+    if (segundos <= 9)
         telaSegundos.innerHTML = "0" + segundos
-    } else if (segundos > 9){
+    else if (segundos > 9)
         telaSegundos.innerHTML = segundos
+}*/
+
+function segundo() {
+    segundos--
+    telaMinutos.innerHTML = minutos;
+
+    if (segundos == 0) {
+        minutos--
+
+        telaMinutos.innerHTML = minutos;
+        segundos = 59;
     }
 
-    
+    //segundos
+    if (segundos <= 9) {
+        telaSegundos.innerHTML = "0" + segundos
 
-    
+    } else if (segundos > 9)
+        telaSegundos.innerHTML = segundos
+
+    //minutos
+    if (minutos <= 9)
+        telaMinutos.innerHTML = "0" + minutos
 }
 
-setInterval(function(){
-    segundo()
-}, 1000)
+var intervalID;
+
+bntIniciar.addEventListener('click', (evento) => {
+
+    console.log("Iniciar");
+
+    intervalID = setInterval(function () {
+        segundo();
+    }, 1000);
+
+
+    bntStop.style.display = "inline";
+    bntIniciar.style.display = "none";
+
+})
+
+bntStop.addEventListener('click', () => {
+  
+    console.log("Parar");
+    clearInterval(intervalID);
+    
+    bntIniciar.style.display = "inline";
+    bntStop.style.display = "none"
+})
+
+
+
+
+
